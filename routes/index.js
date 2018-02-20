41f886a96fcc13c62f65bd372d3c7a266f81ca6e
Samuel Brass <Sam@Samuels-MacBook-Pro.local>
2018-02-20 21:31:01 +1100

    Setting up basic DB stuff

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../db.js");
+var log = require('../helpers/log.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'grid.' });
 });
+
+
 
 module.exports = router;
